712bca4745ac52ddf3640e566c4210f4884a2a0c
petrubear <edison.martinez@gmail.com>
2021-06-14 17:18:22 -0500

    feat: eslint + mongodb init

---
+++ mongodb.js
@@ -0,0 +1,21 @@
+// CRUD
+
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+const connectionURL = 'mongodb://127.0.0.1:27017';
+const databaseName = 'task-manager';
+
+MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
+    if (error) {
+        return console.log('unable to connect to mongodb');
+    }
+
+    const db = client.db(databaseName);
+
+    db.collection('users').insertOne({
+        name: 'Edison',
+        age: 39,
+    });
+});
+
