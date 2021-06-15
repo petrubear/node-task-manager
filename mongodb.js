dc3aac1a7f7b06090b93537fc66a96eb7142b642
petrubear <edison.martinez@gmail.com>
2021-06-15 18:51:55 -0500

    feat: mongo playground code

--- mongodb.js
+++ mongodb.js
@@ -1,10 +1,16 @@
 // CRUD
 
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+// const mongodb = require('mongodb');
+// const MongoClient = mongodb.MongoClient;
+// const ObjectID = mongodb.ObjectID;
+const {MongoClient, ObjectID} = require('mongodb');
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
+
+const id = new ObjectID();
+console.log(id);
+console.log(id.getTimestamp());
 
 MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
     if (error) {
@@ -13,9 +19,57 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
 
     const db = client.db(databaseName);
 
+    // insert single
+    /*
     db.collection('users').insertOne({
         name: 'Edison',
         age: 39,
+    }, (error, result) => {
+        if (error) {
+            return console.log('unable to insert user');
+        }
+
+        console.log(result.ops);
     });
+     */
+    // insert many
+    /*
+    db.collection('users').insertMany([
+        {
+            name: 'Valeria',
+            age: 20,
+        },
+        {
+            name: 'Lorena',
+            age: 50,
+        },
+    ], (error, result) => {
+        if (error) {
+            return console.log('Unable to insert documents');
+        }
+        console.log(result.ops);
+    });
+
+    db.collection('tasks').insertMany([
+        {
+            description: 'ir al dentista',
+            completed: false,
+        },
+        {
+            description: 'inflar llantas',
+            completed: true,
+        },
+        {
+            description: 'calentar la moto',
+            completed: false,
+        },
+    ], (error, result) => {
+        if (error) {
+            return console.log('unable to insert tasks');
+        }
+
+        console.log(result.ops);
+    });
+     */
 });
 
