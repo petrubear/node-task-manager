17ccb2afe9a96209d09a8d25a0c398e4b9a0dc72
petrubear <edison.martinez@gmail.com>
2021-06-16 11:10:16 -0500

    feat: mongoose models & validation

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const connectionURL = 'mongodb://127.0.0.1:27017/task-manager-api';
 
@@ -12,16 +13,49 @@ mongoose.connect(connectionURL, {
 const User = mongoose.model('User', {
     name: {
         type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is not valid');
+            }
+        },
     },
     age: {
         type: Number,
+        required: false,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number');
+            }
+        },
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 7,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Password should not contain password');
+            }
+        },
     },
 });
 
-
+/*
 const me = new User({
     name: 'Edison',
+    email: 'edison.martinez@gmail.COM ',
     age: 39,
+    password: 'abcdef',
 });
 
 me.save().then((result) => {
@@ -29,19 +63,23 @@ me.save().then((result) => {
 }).catch((error) => {
     console.log('Error: ', error);
 });
+*/
 
 const Task = mongoose.model('Task', {
     description: {
         type: String,
+        required: true,
+        trim: true,
     },
     completed: {
         type: Boolean,
+        required: false,
+        default: false,
     },
 });
 
 const task = new Task({
-    description: 'Calentar la moto',
-    completed: false,
+    description: '   Rodar el coche   ',
 });
 
 task.save().then((result) => {
@@ -49,3 +87,4 @@ task.save().then((result) => {
 }).catch((error) => {
     console.log('Error: ', error);
 });
+
