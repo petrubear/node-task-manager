210e815cf8b37d14ec944bf8a8c015cd561e0629
petrubear <edison.martinez@gmail.com>
2021-06-16 19:23:26 -0500

    feat: playground promise chaining

--- playground/promises.js
+++ playground/promises.js
@@ -1,8 +1,8 @@
 const doWorkPromise = new Promise((resolve, reject) => {
     setTimeout(() => {
-        // resolve([7, 8, 9]);
-        reject('things went wrong');
-    }, 2000);
+        resolve([7, 8, 9]);
+        // reject(new Error('things went wrong'));
+    }, 1000);
 });
 
 
@@ -10,4 +10,37 @@ doWorkPromise.then((result) => {
     console.log('succes ', result);
 }).catch((error) => {
     console.log(error);
+});
+
+
+// ////////////////////////
+
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a + b);
+        }, 2000);
+    });
+};
+
+add(1, 2).then((sum) => {
+    console.log(sum);
+    add(sum, 5).then((sum2) => {
+        console.log(sum2);
+    }).catch((e) => {
+        console.log(e);
+    });
+}).catch((e) => {
+    console.log(e);
+});
+
+// with promise chaining
+
+add(2, 3).then((sum) => {
+    console.log(sum);
+    return add(sum, 4);
+}).then((sum2) => {
+    console.log(sum2);
+}).catch((e) => {
+    console.log(e);
 });
