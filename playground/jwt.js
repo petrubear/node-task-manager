0582d1b3a4e48af7c1911cd2d8311a919da84624
petrubear <edison.martinez@gmail.com>
2021-06-20 12:18:17 -0500

    feat: jwt playground

---
+++ playground/jwt.js
@@ -0,0 +1,11 @@
+const jwt = require('jsonwebtoken');
+const myFunction = async () => {
+    const key = 'thisismynewcourse';
+    const token = jwt.sign({_id: 'abc234'}, key, {expiresIn: '5 seconds'});
+    console.log(token);
+
+    const data = jwt.verify(token, key);
+    console.log(data);
+};
+
+myFunction().then().catch((e) => console.log(e));
