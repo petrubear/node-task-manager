90d265a2bcc8ee7b111e0b56788d3baeb7806a1b
petrubear <edison.martinez@gmail.com>
2021-06-20 11:16:00 -0500

    feat: bcrypt playground

---
+++ playground/bcrypt.js
@@ -0,0 +1,13 @@
+const bcrypt = require('bcryptjs');
+const myfunction = async () => {
+    const password = 'Red12345!';
+    const hashedPassword = await bcrypt.hash(password, 8);
+
+    console.log((password));
+    console.log(hashedPassword);
+
+    const isMatch = await bcrypt.compare('red12345!', hashedPassword);
+    console.log(isMatch);
+};
+
+myfunction().then().catch((e) => console.log(e));
