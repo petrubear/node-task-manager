8367453a2f4fda07ae3fa58ecd43483411861b09
petrubear <edison.martinez@gmail.com>
2021-06-21 20:42:12 -0500

    refactor: getUserProfile to toJSON

--- src/routers/user.js
+++ src/routers/user.js
@@ -10,7 +10,7 @@ router.post('/users', async (req, res) => {
     try {
         await user.save();
         const token = await user.generateAuthToken();
-        res.status(201).send({user: user.getPublicProfile(), token});
+        res.status(201).send({user, token});
     } catch (e) {
         console.log(e);
         res.status(400).send(e);
@@ -21,7 +21,7 @@ router.post('/users/login', async (req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password);
         const token = await user.generateAuthToken();
-        res.send({user: user.getPublicProfile(), token});
+        res.send({user, token});
     } catch (e) {
         res.status(400).send(e);
     }
