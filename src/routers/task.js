71dbffa285126e854f065cd8f0b9f6ce2c9c08ac
petrubear <edison.martinez@gmail.com>
2021-06-23 18:21:34 -0500

    feat: query filters

--- src/routers/task.js
+++ src/routers/task.js
@@ -20,11 +20,22 @@ router.post('/tasks', auth, async (req, res) => {
     }
 });
 
+// GET /tasks?completed=true|false
 router.get('/tasks', auth, async (req, res) => {
     try {
-        const owner = req.user._id;
-        const tasks = await Task.find({owner: owner});
-        res.send(tasks);
+        // const owner = req.user._id;
+        // const tasks = await Task.find({owner: owner});
+        // res.send(tasks);
+
+        const match = {};
+        if (req.query.completed) {
+            match.completed = req.query.completed === 'true';
+        }
+        await req.user.populate({
+            path: 'tasks',
+            match,
+        }).execPopulate();
+        res.send(req.user.tasks);
     } catch (e) {
         res.status(500).send(e);
     }
